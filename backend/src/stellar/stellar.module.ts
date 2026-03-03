072464ce52a40aeb1461ef1811b16aa92b75a6d9
ahmadogo <abdulkareemahmad413@gmail.com>
2026-03-03 22:09:45 +0100

    fixed lint plus e2e test  errors

--- backend/src/stellar/stellar.module.ts
+++ backend/src/stellar/stellar.module.ts
@@ -1,16 +1,7 @@
 import { Module } from "@nestjs/common";
 import { StellarService } from "./stellar.service";
 import { StellarValidator } from "./utils/stellar.validator";
-import {
-  StellarExceptionFilter,
-  StellarException,
-  StellarAccountNotFoundException,
-  StellarPaymentNotFoundException,
-  StellarAddressInvalidException,
-  HorizonApiException,
-  SorobanRpcException,
-  StellarNetworkConfigException,
-} from "./exceptions/stellar.exceptions";
+
 
 @Module({
   providers: [
@@ -23,14 +14,6 @@ import {
   exports: [
     StellarService,
     "STELLAR_VALIDATOR",
-    StellarExceptionFilter,
-    StellarException,
-    StellarAccountNotFoundException,
-    StellarPaymentNotFoundException,
-    StellarAddressInvalidException,
-    HorizonApiException,
-    SorobanRpcException,
-    StellarNetworkConfigException,
   ],
 })
 export class StellarModule {}
